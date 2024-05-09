dcca1cb2685ead99f5204ed846834f2bf6575b72
jaspreet-27 <148739147+jaspreet-27@users.noreply.github.com>
2024-05-09 15:00:43 +0530

    Delete users directory

--- users/src/email/email.ts
+++
@@ -1,8 +0,0 @@
-import nodemailer from 'nodemailer';
-export const mailTransporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'mailto:jaspreetkaurwins@gmail.com',
-    pass: 'carq velv hjjl kdpv',
-  },
-});
--- users/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-// import { Express ,Router} from 'express';
-// import userRoute from './user/userRoutes';
-
-// Router.use('/user', userRoute);
-
-// export default router;
